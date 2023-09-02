143af383fe1e7a7c6aee8969d117d29f3a8276bf
Saurabh Singh <sibsaurabh@gmail.com>
2023-09-03 01:58:45 +0530

    implemented proof verification and generation

--- client/index.js
+++ client/index.js
@@ -1,17 +1,36 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to prompt user for input
+function promptInput(question) {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+}
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  const name = await promptInput('Enter the name to send the gift : ');
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name : name,
+    proof : proof
   });
 
   console.log({ gift });
+  rl.close();
 }
 
 main();
